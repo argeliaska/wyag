6f5a4b22c2996361d8a352307b3a31fbc3e071f3
Argelia Skakievich <argeliaska@gmail.com>
2023-08-26 14:19:39 -0600

    [FEAT] init command added

--- libwyag.py
+++ libwyag.py
@@ -1,6 +1,5 @@
 import argparse
 import collections
-import configparser
 from datetime import datetime
 import grp, pwd
 from fnmatch import fnmatch
@@ -11,11 +10,21 @@ import re
 import sys
 import zlib
 
+from .gitrepository import repo_create
+
+
 argparse = argparse.ArgumentParser(description="The stupidest content tracker")
 
 # you don't just call git, you call git COMMAND
 argsubparsers = argparse.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
+
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument("path", 
+                   metavar="directory", 
+                   nargs="?", 
+                   default=".", 
+                   help="Where to create the repository.")
 
 def main(argv=sys.argv[1:]):
     args = argparse.parse_args(argv)
@@ -25,7 +34,7 @@ def main(argv=sys.argv[1:]):
     elif args.command == "checkout"     : pass
     elif args.command == "commit"       : pass
     elif args.command == "hash-object"  : pass
-    elif args.command == "init"         : pass
+    elif args.command == "init"         : cmd_init(args)
     elif args.command == "log"          : pass
     elif args.command == "ls-files"     : pass
     elif args.command == "merge"        : pass
@@ -36,3 +45,7 @@ def main(argv=sys.argv[1:]):
     elif args.command == "status"       : pass
     elif args.command == "tag"          : pass
     else                                : print("Bad command.")
+
+
+def cmd_init(args):
+    repo_create(args.path)
