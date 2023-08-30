e33619e6c2f45f5f87795e173e73ac2902176d03
Argelia Skakievich <argeliaska@gmail.com>
2023-08-29 18:38:44 -0600

    [FEAT] refs functions added

--- gitobject_utils.py
+++ gitobject_utils.py
@@ -1,7 +1,8 @@
 import os
 import zlib
 import hashlib
-from .gitrepository import repo_file
+import collections
+from .gitrepository import repo_dir, repo_file
 from .gitobject import GitCommit, GitTree, GitTag, GitBlob
 
 def object_read(repo, sha):
@@ -100,3 +101,50 @@ def ls_tree(repo, ref, recursive=None, prefix=""):
             ))
         else: # This is a branch, recurse
             ls_tree(repo, item.sha, recursive, os.path.join(prefix, item.path))
+
+
+def ref_resolve(repo, ref):
+    path = repo_file(repo, ref)
+
+    # Sometimes, an inderect reference may be broken. This is normal
+    # in one specific case: we're looking for HEAD on a new repository
+    # with no commits. In that case, .git/HEAD points to "ref:
+    # refs/heads/main", but .git/refs/heads/main doesn't exist yet
+    # (since there's no commit for it to refer to).
+
+    if not os.path.isfile(path):
+        return None
+    
+    with open(path, 'r') as fp:
+        data = fp.read()[:-1]
+        # Drop final \n ^^^^^
+    if data.startswith("ref: "):
+        return ref_resolve(repo, data[5:])
+    else:
+        return data
+    
+def ref_list(repo, path=None):
+    if not path:
+        path = repo_dir(repo, "refs")
+    ret = collections.OrderedDict()
+    # Git show refs sorted. To do the same, we use
+    # an OrderedDict and sort the output of listdir
+    for f in sorted(os.listdir(path)):
+        can = os.path.join(path, f)
+        if os.path.isdir(can):
+            ret[f] = ref_list(repo, can)
+        else:
+            ret[f] = ref_resolve(repo, can)
+
+    return ret
+
+def show_ref(repo, refs, with_hash=True, prefix=""):
+    for k, v in refs.items():
+        if type(v) == str:
+            print("{0}{1}{2}".format(
+                v + " " if with_hash else "",
+                prefix + "/" if prefix else "",
+                k))
+        else:
+            show_ref(repo, v, with_hash=with_hash, 
+                     prefix="{0}{1}{2}".format(prefix, "/" if prefix else "", k))
